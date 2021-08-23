0981c5761e7d3cf012a653c80c85b23ff9d7b557
HungBa <nhuhung95@gmail.com>
2021-08-24 03:35:45 +0700

    Refactor code: creat routers, app/controllers folders

--- src/index.js
+++ src/index.js
@@ -5,11 +5,21 @@ const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
+const route = require('./routes')
+
 // Static Files
 app.use(express.static(path.join(__dirname, 'public')))
 
+// Xử lý kết quả trả về cho việc submit từ form
+app.use(express.urlencoded({
+  extended: true
+}))
+
+// Xử lý kết quả trả về cho việc submit từ dạng khác axios, ajax, XMLHtt,...
+app.use(express.json())
+
 // HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 // Template handlebars
 
@@ -22,15 +32,8 @@ app.engine('hbs', exphbs({
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources/views'))
 
-app.get('/', (req, res) => {
-  // res.send('Hello World!')
-  res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  // res.send('Hello World!')
-  res.render('news');
-})
+// Routes init
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
